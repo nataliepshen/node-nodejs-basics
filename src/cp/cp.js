33cf1c99b4fd857b93b5d48462ae4ce3a87c42cb
nataliepshen <romashki23@mail.ru>
2022-06-05 19:38:58 +0300

    done cp module

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,13 @@
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import * as path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    let masterPath = path.join(__dirname, 'files', 'script.js');
+    const chilpProcess = fork(masterPath, process.argv.slice(2));
 };
+spawnChildProcess();
